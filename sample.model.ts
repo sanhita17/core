5909429810b465fa32cded3cf9e3b8ae8dbb3355
sanhita17 <taneeru.sanhita@gmail.com>
2017-08-16 23:14:13 -0500

    angular2 sample model

---
+++ sample.model.ts
@@ -0,0 +1,31 @@
+export class RssModel  {
+    clientId: string;
+    alienNmbr: string;
+    intlCashAsstBnftAmt: number;
+    intlCashAsstTypeCode:string;
+    intlSnapBftAmt:number;
+    intakeDate:Date;
+    englishLevel:string;
+
+    caseClsoureDate:Date;
+    caseClosureReasonCode:string;
+    clsrCashAsstTypeCode:string;
+    clsrSnapBftAmt:number;
+    clsrCashAsstBnftAmt:number;
+
+    jobStartDate:Date;
+    jobTypeCode:string;
+    emplrName:string;
+    emplrAddr:string;
+    emplrContact:string;
+    emplrPhone:number;
+    emplrEmailAddr:string;
+    jobTitle:string;
+    hrsPerWeek:number;
+    fullTimeInd:string;
+    hrlyWage:number;
+    bnftPresentInd:string;
+    mnthlyWage:number;
+    pgmSrcCode:string;
+
+   }
